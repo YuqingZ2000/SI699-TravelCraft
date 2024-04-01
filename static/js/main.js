2013f46bb234851493af33c6babe1734761b9aac
YuqingZ2000 <113810156+YuqingZ2000@users.noreply.github.com>
2024-04-01 18:21:23 -0400

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -70,3 +70,64 @@
   }, true)
 
 })()
+
+/* Updated! */
+// Display driving time only when car is selected
+document.addEventListener('DOMContentLoaded', function() {
+  var travelMethodSelect = document.getElementById('travel_method');
+  var drivingTimeField = document.querySelector('.form-driving');
+
+  // Check the initial value of the travel method select element
+  if (travelMethodSelect.value === 'Car') {
+    drivingTimeField.style.display = 'block';
+  }
+
+  // Add event listener to listen for changes in travel method
+  travelMethodSelect.addEventListener('change', function() {
+    if (this.value === 'Car') {
+      drivingTimeField.style.display = 'block';
+    } else {
+      drivingTimeField.style.display = 'none';
+    }
+  });
+});
+
+// Add and remove departure cities, Error in autocomplete!
+document.addEventListener('DOMContentLoaded', function() {
+  var departureCityCount = 1;
+  var maxDepartureCities = 4; // Maximum number of departure cities allowed
+
+  function createDepartureCity() {
+      departureCityCount++;
+      var newDepartureCity = document.createElement('div');
+      newDepartureCity.classList.add('form-group');
+      newDepartureCity.classList.add('form-departure' + departureCityCount);
+      newDepartureCity.innerHTML = `
+          <div class="departure-city-field">
+              <input type="text" name="departure${departureCityCount}" class="form-control" id="autocompleteDatalist${departureCityCount}" data-datalist="departure-city" placeholder="Departure City ${departureCityCount}" />
+              <datalist id="departure-city${departureCityCount}">
+                  {% for city in departure_list %}
+                  <option>{{city}}</option>
+                  {% endfor %}
+              </datalist>
+          </div>
+      `;
+      document.getElementById('additional-departure-cities').appendChild(newDepartureCity);
+  }
+
+  document.getElementById('add-departure-city').addEventListener('click', function() {
+      if (departureCityCount < maxDepartureCities) {
+          createDepartureCity();
+      } else {
+          alert('You can only add up to ' + maxDepartureCities + ' departure cities.');
+      }
+  });
+
+  document.getElementById('remove-departure-city').addEventListener('click', function() {
+      var additionalDepartureCities = document.getElementById('additional-departure-cities');
+      if (departureCityCount > 1) {
+          departureCityCount--;
+          additionalDepartureCities.removeChild(additionalDepartureCities.lastChild);
+      }
+  });
+});
